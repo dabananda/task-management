eaab1b443a9f6d91417982277f7536113b42c7a2
dabananda <imdmitra@gmail.com>
2025-01-15 01:44:51 +0600

    Added postgresql

--- task_management/settings.py
+++ task_management/settings.py
@@ -72,13 +72,25 @@ TEMPLATES = [
 WSGI_APPLICATION = 'task_management.wsgi.application'
 
 
-# Database
+# Database - SQL Lite
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# Database - PostgreSQL
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'task_management',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
