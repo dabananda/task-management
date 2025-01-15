951b702ed07aaa4af5bbf7f7bc1f8ef43a1a230c
dabananda <imdmitra@gmail.com>
2025-01-15 23:38:20 +0600

    Added models and relationship

--- tasks/models.py
+++ tasks/models.py
@@ -1,9 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+  name = models.CharField(max_length=100)
+  email = models.EmailField(unique=True)
+
+class Project(models.Model):
+  name = models.CharField(max_length=100)
+  start_date = models.DateField()
+
 class Task(models.Model):
+  project = models.ForeignKey(Project, on_delete=models.CASCADE, default=1)
+  employee = models.ManyToManyField(Employee)
   title = models.CharField(max_length=100)
   description = models.TextField()
   is_completed = models.BooleanField(default=False)
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
+
+class TaskDetails(models.Model):
+  LOW = 'L'
+  MEDIUM = 'M'
+  HIGH = 'H'
+  PRIORITY_OPTIONS = (
+    (LOW, 'Low'),
+    (MEDIUM, 'Medium'),
+    (HIGH, 'High'),
+  )
+  task = models.OneToOneField(Task, on_delete=models.CASCADE)
+  assigned_to = models.CharField(max_length=100)
+  priority = models.CharField(max_length=1, choices=PRIORITY_OPTIONS, default=LOW)
