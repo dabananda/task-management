2af2d9512b869aa5517356c1b2b68a1c080a9e94
dabananda <imdmitra@gmail.com>
2025-01-16 13:59:52 +0600

    Added forms

--- tasks/models.py
+++ tasks/models.py
@@ -14,6 +14,7 @@ class Task(models.Model):
   title = models.CharField(max_length=100)
   description = models.TextField()
   is_completed = models.BooleanField(default=False)
+  due_date = models.DateField(null=True, blank=True)
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
 
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from tasks.forms import TaskForm
+from tasks.models import Employee, Task
 
 # Create your views here.
 def admin_dashboard(request):
@@ -7,5 +10,28 @@ def admin_dashboard(request):
 def user_dashboard(request):
   return render(request, "dashboard/user-dashboard.html")
 
-def test(request):
-  return render(request, "test.html")
+def create_task(request):
+  employees = Employee.objects.all()
+  form = TaskForm(employees=employees)
+
+  if request.method == "POST":
+    form = TaskForm(request.POST, employees=employees)
+    if form.is_valid():
+      data = form.cleaned_data
+      title = data.get("title")
+      description = data.get("description")
+      due_date = data.get("due_date")
+      assigned_to = data.get("assigned_to")
+      task = Task.objects.create(title=title, description=description, due_date=due_date)
+
+      for emp_id in assigned_to:
+        emp = Employee.objects.get(id=emp_id)
+        # task.assign_to.add(emp)
+        task.employee.add(emp)
+      
+      return HttpResponse("Task created successfully")
+
+  context = {
+    "form": form
+  }
+  return render(request, "task_form.html", context)
