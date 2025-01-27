1a9941b31830dae51ebcad6e77886f8ad15b443b
dabananda <imdmitra@gmail.com>
2025-01-27 18:58:48 +0600

    Added forms and styles

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from tasks.models import Task
 
 class TaskForm(forms.Form):
   title = forms.CharField(max_length=250, label="Task Title")
@@ -12,3 +13,58 @@ class TaskForm(forms.Form):
     self.fields['assigned_to'].choices = [
       (emp.id, emp.name) for emp in employees
     ]
+
+class StyledFormMixing:
+  form_style_1 = "border border-gray-400 w-full px-2 py-1 mb-2"
+  form_style_2 = "border border-gray-400 px-2 py-1 mb-2"
+
+  def apply_styled_widgets(self):
+    for field_name, field in self.fields.items():
+      if isinstance(field.widget, forms.TextInput):
+        field.widget.attrs.update({
+          'class': self.form_style_1,
+          'placeholder': f"Enter {field.label.lower()}"
+        })
+      elif isinstance(field.widget, forms.Textarea):
+        field.widget.attrs.update({
+          'class': self.form_style_1,
+          'placeholder': f"Enter {field.label.lower()}",
+          'rows': 5
+        })
+      elif isinstance(field.widget, forms.SelectDateWidget):
+        field.widget.attrs.update({
+          'class': self.form_style_2
+        })
+      elif isinstance(field.widget, forms.CheckboxSelectMultiple):
+        field.widget.attrs.update({
+          'class': self.form_style_2
+        })
+
+class TaskModelForm(StyledFormMixing, forms.ModelForm):
+  class Meta:
+    model = Task
+    fields = ['title', 'description', 'due_date', 'assigned_to']
+    widgets = {
+      'due_date': forms.SelectDateWidget,
+      'assigned_to': forms.CheckboxSelectMultiple
+    }
+    # widgets = {
+    #   'title': forms.TextInput(attrs={
+    #     'class': "border border-gray-400 w-full px-2 py-1 mb-2",
+    #     'placeholder': "Task title"
+    #   }),
+    #   'description': forms.Textarea(attrs={
+    #     'class': "border border-gray-400 w-full px-2 py-1 mb-2",
+    #     'placeholder': "Task description"
+    #   }),
+    #   'due_date': forms.SelectDateWidget(attrs={
+    #     'class': "border border-gray-400 px-2 py-1 mb-2"
+    #   }),
+    #   'assigned_to': forms.CheckboxSelectMultiple(attrs={
+    #     'class': "border border-gray-400 px-2 py-1 mb-2"
+    #   })
+    # }
+
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.apply_styled_widgets()
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from tasks.forms import TaskForm
+from tasks.forms import TaskForm, TaskModelForm
 from tasks.models import Employee, Task
 from django.http import HttpResponse
 
@@ -15,19 +15,24 @@ def test(request):
 
 def create_task(request):
   employees = Employee.objects.all()
-  form = TaskForm(employees = employees)
+  form = TaskModelForm()
   if request.method == 'POST':
-    form = TaskForm(request.POST, employees = employees)
+    form = TaskModelForm(request.POST)
     if form.is_valid():
-      data = form.cleaned_data
-      title = data.get('title')
-      description = data.get('description')
-      due_date = data.get('due_date')
-      assigned_to = data.get('assigned_to')
-      task = Task.objects.create(title = title, description = description, due_date = due_date)
-      for emp_id in assigned_to:
-        employee = Employee.objects.get(id = emp_id)
-        task.assigned_to.add(employee)
+      ''' For Django Model Form Data'''
+      form.save()
+      return render(request, 'task_form.html', {'form': form, 'message': 'Task ceated successfully!'})
+
+      ''' For Django Form Data '''
+      # data = form.cleaned_data
+      # title = data.get('title')
+      # description = data.get('description')
+      # due_date = data.get('due_date')
+      # assigned_to = data.get('assigned_to')
+      # task = Task.objects.create(title = title, description = description, due_date = due_date)
+      # for emp_id in assigned_to:
+      #   employee = Employee.objects.get(id = emp_id)
+      #   task.assigned_to.add(employee)
       return HttpResponse("Task created successfully!")
   context = {'form': form}
   return render(request, "task_form.html", context)
