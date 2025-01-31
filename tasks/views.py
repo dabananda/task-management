e99b59b7945d58657bbbf5a2b891019f0e889095
dabananda <imdmitra@gmail.com>
2025-02-01 00:20:06 +0600

    Made the admin dashboard dynamic

--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
+from django.db.models import Count, Q
 from tasks.forms import TaskForm, TaskModelForm
 from tasks.models import Employee, Task
 from django.http import HttpResponse
 
 def admin_dashboard(request):
-  return render(request, "dashboard/admin-dashboard.html")
+  tasks = Task.objects.select_related('details').prefetch_related('assigned_to').all()
+  # total_task = tasks.count()
+  # pending_tasks = Task.objects.filter(status = 'PENDING').count()
+  # in_progress_tasks = Task.objects.filter(status = 'IN_PROGRESS').count()
+  # completed_tasks = Task.objects.filter(status = 'COMPLETED').count()
+  counts = Task.objects.aggregate(
+    total=Count('id'),
+    completed=Count('id', filter=Q(status='COMPLETED')),
+    in_progress=Count('id', filter=Q(status='IN_PROGRESS')),
+    pending=Count('id', filter=Q(status='PENDING')),
+  )
+  context = {
+    "tasks": tasks,
+    "counts": counts
+  }
+  return render(request, "dashboard/admin-dashboard.html", context)
 
 def user_dashboard(request):
   return render(request, "dashboard/user-dashboard.html")
