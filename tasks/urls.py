2a10315958fc8af2f100b56134769e7d6a12dd44
dabananda <imdmitra@gmail.com>
2025-02-05 10:12:59 +0600

    Implemented Task and TaskDetails Update Methods

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
   path('user-dashboard/', user_dashboard),
   path('test/', test),
   path('create-task/', create_task, name='create-task'),
-  path('view_task/', view_task)
+  path('view-task/', view_task),
+  path('update-task/<int:id>/', update_task, name="update-task")
 ]
